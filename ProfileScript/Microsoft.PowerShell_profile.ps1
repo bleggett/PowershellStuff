6bd266b59c838908313b980272ecd8b78d8d34d2
bleggett <algosystem@gmail.com>
2014-08-06 10:23:22 -0400

    Update powershell profile

--- ProfileScript/Microsoft.PowerShell_profile.ps1
+++ ProfileScript/Microsoft.PowerShell_profile.ps1
@@ -5,18 +5,16 @@ Set-Alias less more
 Set-Alias help get-help
 Add-PSSnapin Microsoft.TeamFoundation.PowerShell
 
-#Set environment variables for Visual Studio Command Prompt
-pushd 'C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC'
-cmd /c “vcvarsall.bat&set” |
+#Set environment variables for Visual Studio Command Prompt.
+#This method uses a static text file with the output of vcvarsall.bat, which speeds startup time.
+#This assumes regen-vc-vars has been run at least one time.
+cat $env:userprofile/Documents/WindowsPowerShell\VCVars.txt |
 foreach {
-  if ($_ -match “=”) {
     $v = $_.split(“=”); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
 }
-popd
 write-host "Visual Studio 2013 Command Prompt variables set.`n" -ForegroundColor Yellow
 
-
+#Custom prompt
 function prompt
 {
 write-host "$env:username" -ForegroundColor Magenta -nonewline
@@ -25,10 +23,19 @@ write-host "$([System.Net.Dns]::GetHostName()) [$(Get-Location)]$" -ForegroundCo
 return ' '
 }
 
-function grep ($input)
+#Regenerates the VCVars.txt file in the user profile folder.
+function regen-vc-vars
 {
-
+pushd "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC"
+cmd /c 'vcvarsall.bat&set' | out-file $env:userprofile/Documents/WindowsPowerShell\VCVars.txt
+popd
+write-host "Successfully regenerated list of Visual Studio 2013 Command Prompt environment variables" -foregroundcolor green
 }
+
+#function grep ($input)
+#{
+#
+#}
 #function Bulk-File-Rename ($replace, $with)
 #{
 #	Dir | Rename-Item -NewName { $_.name -replace $replace $with }	
