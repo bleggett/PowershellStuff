6b1b596b5882f40c39cbbadd0e92cf449337f7bd
Benjamin Leggett <algosystem@gmail.com>
2014-12-10 10:33:14 -0500

    Add recursive text replace script

---
+++ RecursiveReplace/recursiveReplace.ps1
@@ -0,0 +1,11 @@
+﻿$match = "..\\..\\CommonAssemblyInfo.cs"
+$replace = "..\..\..\Build\CommonAssemblyInfo.cs"
+
+$files = Get-ChildItem $(get-location) -include *.csproj -Recurse 
+
+foreach($file in $files) 
+{ 
+    ((Get-Content $file.fullname) -creplace $match, $replace) | set-content $file.fullname 
+}
+
+read-host -prompt "Done! Press any key to close."
